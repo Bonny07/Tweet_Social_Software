c1a04600bde304fdaa38907e1444f1dfd940ae41
Bonny07 <bonny2992352480@outlook.com>
2024-07-03 13:32:18 +0800

    git_init

---
+++ app/src/main/java/com/baochenglin/pproject/HomeFragment.java
@@ -0,0 +1,29 @@
+package com.baochenglin.pproject;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HomeFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        // 你需要创建一个适配器类来处理数据的绑定
+        // recyclerView.setAdapter(yourAdapter);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/baochenglin/pproject/NotificationsFragment.java
@@ -0,0 +1,17 @@
+package com.baochenglin.pproject;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class NotificationsFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
