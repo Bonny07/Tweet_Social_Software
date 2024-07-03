409d325b35e21374b40f758f2ce0978f61211243
Bonny07 <111042029+Bonny07@users.noreply.github.com>
2024-07-03 15:17:47 +0800

    complete the main menu layout

--- app/src/main/java/com/baochenglin/pproject/EditFragment.java
+++ app/src/main/java/com/baochenglin/pproject/EditFragment.java
@@ -12,10 +12,10 @@ public class EditFragment extends Fragment {
         // Required empty public constructor
     }
 
-    @Override
+/*    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_edit, container, false);
-    }
+    }*/
 }
