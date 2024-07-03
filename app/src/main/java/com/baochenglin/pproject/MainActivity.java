bb71762081eb5a7abe5d821bf8d767e65e237dde
Bonny07 <111042029+Bonny07@users.noreply.github.com>
2024-07-03 14:15:34 +0800

    Initial commit

--- app/src/main/java/com/baochenglin/pproject/MainActivity.java
+++ app/src/main/java/com/baochenglin/pproject/MainActivity.java
@@ -1,95 +1,55 @@
 package com.baochenglin.pproject;
 
-import android.os.Bundle;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
-import android.view.Menu;
+import androidx.fragment.app.FragmentManager;
+import android.os.Bundle;
 import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final FragmentManager fragmentManager = getSupportFragmentManager();
+    private final Fragment homeFragment = new HomeFragment();
+    private final Fragment searchFragment = new SearchFragment();
+    private final Fragment notificationsFragment = new NotificationsFragment();
+    private final Fragment editFragment = new EditFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 设置底部导航
-        BottomNavigationView bottomNav = findViewById(R.id.nav_view);
-        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
 
-        // 确保活动首次打开时显示主页Fragment
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new HomeFragment()).commit();
-        }
-
-        // 设置ActionBar，添加个人主页和设置按钮
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true); // 显示左上角的返回按钮
-            actionBar.setHomeAsUpIndicator(R.drawable.ic_home); // 将返回按钮设置为个人主页图标
-        }
-    }
-
-    // 底部导航栏的监听器
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
-
-                    switch (item.getItemId()) {
-                        case R.id.navigation_home:
-                            selectedFragment = new HomeFragment();
-                            break;
-                        case R.id.navigation_search:
-                            selectedFragment = new SearchFragment();
-                            break;
-                        case R.id.navigation_notifications:
-                            selectedFragment = new NotificationsFragment();
-                            break;
-                        case R.id.navigation_inbox:
-                            selectedFragment = new InboxFragment();
-                            break;
-                        case R.id.navigation_settings:
-                            selectedFragment = new SettingsFragment();
-                            break;
-                    }
-
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            selectedFragment).commit();
-                    return true;
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        selectedFragment = homeFragment;
+                        break;
+                    case R.id.navigation_search:
+                        selectedFragment = searchFragment;
+                        break;
+                    case R.id.navigation_notifications:
+                        selectedFragment = notificationsFragment;
+                        break;
+                    case R.id.navigation_edit:
+                        selectedFragment = editFragment;
+                        break;
                 }
-            };
 
-    // 创建顶部菜单
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.top_menu, menu);
-        return true;
-    }
+                if (selectedFragment != null) {
+                    fragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                }
+                return true;
+            }
+        });
 
-    // 处理顶部菜单项的点击事件
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                // 跳转到个人主页Fragment
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new ProfileFragment()).commit();
-                return true;
-            case R.id.menu_settings:
-                // 跳转到设置Fragment
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new SettingsFragment()).commit();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
     }
 }
--- app/src/main/java/com/baochenglin/pproject/NotificationsFragment.java
+++ app/src/main/java/com/baochenglin/pproject/NotificationsFragment.java
@@ -7,11 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
-public class NotificationsFragment extends AppCompatActivity {
+public class NotificationsFragment extends Fragment {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+
 }
